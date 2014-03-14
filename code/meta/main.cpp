bda9624e0a100f804400f71805398c6505777cb5
Anthony L'Huede <a.lhuede@gmail.com>
2014-03-13 23:39:27 -0700

    split off into type_inl, untested construct / copy / move / destruct functions in type

--- code/meta/main.cpp
+++ code/meta/main.cpp
@@ -6,15 +6,23 @@
 #include <cassert>
 #include <sstream>
 #include <stack>
+#include <memory>
 
 #include "json\json.h"
 
 template <class T> using vector = std::vector<T>;
+template <class T> using shared_ptr = std::shared_ptr<T>;
+template <class T, class U> using hash_map = std::unordered_map<T, U>;
 typedef std::string string;
+typedef std::istream istream;
+typedef std::ostream ostream;
+typedef std::ostringstream ostringstream;
+typedef std::istringstream istringstream;
 
 #include "meta\meta.h"
 
 unsigned meta::type::S_ID_COUNTER = 0;
+hash_map<string, const meta::type*> meta::meta_registry::m_registry;
 
 DECLARE_META_TYPE(int);
 DECLARE_META_TYPE(bool);
@@ -24,6 +32,7 @@ DECLARE_META_TYPE(string);
 
 class test_class { public: test_class(int ti, string ts) : test_int(ti), test_string(ts) {} int test_int; string test_string; };
 
+DECLARE_META_OBJECT(meta::nulltype);
 DECLARE_META_OBJECT(test_class);
 
 class serializer
@@ -31,10 +40,10 @@ class serializer
 public:
   virtual ~serializer() = 0 {};
   
-  virtual void add(const meta::type& t, void* obj) = 0;
-  virtual void write(std::ostream& stream) = 0;
-  virtual bool read(std::istream& stream) = 0;
-  virtual bool get(const string& var, void* obj) = 0;
+  virtual void add(const string& name, const meta::type& t, void* obj) = 0;
+  virtual void write(ostream& stream) = 0;
+  virtual bool read(istream& stream) = 0;
+  virtual shared_ptr<void> get(const string& var, const meta::type& t) = 0;
 };
 
 class JSonSerializer : public serializer
@@ -44,44 +53,66 @@ public:
   ~JSonSerializer() {}
 
   template <class T>
-  void add(const T& obj)
+  void add(const string& name, const T& obj)
   {
-    add(meta::typeof(obj), const_cast<T*>(&obj));
+    add(name, meta::typeof(obj), const_cast<T*>(&obj));
   }
-  virtual void add(const meta::type& t, void* obj)
+  virtual void add(const string& name, const meta::type& t, void* obj)
   {
-    m_root.append(recursive_add(t, obj));
+    m_root[name] = recursive_add(t, obj);
   }
-  virtual void write(std::ostream& stream)
+  virtual void write(ostream& stream)
   {
     Json::StyledStreamWriter writer;
     writer.write(stream, m_root);
   }
-  virtual bool read(std::istream& stream)
+  virtual bool read(istream& stream)
   {
     Json::Reader reader;
     if (reader.parse(stream, m_root, false) == false)
       return false;
 
-    recursive_read(m_root);
+    Json::Value::Members members = m_root.getMemberNames();
+    for (auto it = members.begin(); it != members.end(); ++it)
+    {
+      m_loadedVariables[*it] = m_root.get(*it, "");
+    }
     return true;
   }
-  virtual bool get(const string& var, void* obj)
+
+  virtual shared_ptr<void> get(const string& var, const meta::type& t)
   {
-    
+    auto it = m_loadedVariables.find(var);
+    if (it == m_loadedVariables.end())
+      return shared_ptr<void>(nullptr);
+
+    Json::Value& value = it->second;
+    //value.
+
     return false;
   }
 
 private:
-  void recursive_read(Json::Value node)
+  void recursive_read(const Json::Value& node)
   {
-    Json::Value::Members members = node.getMemberNames();
-    
-    for (size_t i = 0; i < node.size(); ++i)
+    //Json::Value::Members members = node.getMemberNames();
+    if (node.isObject())
     {
-      m_loadedVariables[members[i]] = node[i];
-      recursive_read(node[i]);
+      
     }
+    else
+    {
+      //m_loadedVariables[]
+      assert(false);
+    }
+    //else if (node.isArray())
+    //{
+    //  for (size_t i = 0; i < node.size(); ++i)
+    //  {
+    //    //m_loadedVariables[members[i]] = node[i];
+    //    recursive_read(node[i]);
+    //  }
+    //}
   }
   Json::Value recursive_add(const meta::type& t, void* obj)
   {
@@ -113,7 +144,7 @@ private:
   }
   
   Json::Value m_root;
-  std::unordered_map<string, Json::Value> m_loadedVariables;
+  hash_map<string, Json::Value> m_loadedVariables;
 };
 
 int main(void)
@@ -124,7 +155,7 @@ int main(void)
   float ffff = 12.5f;
   float fffresult = meta::converter::toFloat(meta::typeof(ffff), &ffff);
 
-  meta::type& t = meta::meta_holder<test_class>::t;
+  meta::type& t = const_cast<meta::type&>(meta::meta_holder<test_class>::t);
   t.Field("test_string", &test_class::test_string);
   t.Field("test_int", &test_class::test_int);
   const vector<const meta::field*>& fields = t.fields();
@@ -134,8 +165,8 @@ int main(void)
   std::ofstream writefile;
   writefile.open("test.json");
   JSonSerializer serializer;
-  serializer.add(tc);
-  serializer.add(tc);
+  serializer.add("test_class1", tc);
+  serializer.add("test_class2", tc);
   serializer.write(writefile);
   writefile.close();
 
