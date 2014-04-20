faac0b556ac648248465bbb393c39bd4dfe05990
Anthony L'Huede <a.lhuede@gmail.com>
2014-04-19 18:37:51 -0700

    moved read/write and tostring code into operations_inl

--- code/meta/_operations_inl.h
+++ code/meta/_operations_inl.h
@@ -42,4 +42,125 @@ template <class T> T& ConvertMemory(void* memory)
 {
   return *static_cast<T*>(memory);
 }
+
+template <class T>
+void Serialize(serializer* s, const string& name, const T& object)
+{
+  typeof<T>()->Serialize(s, name, &object);
+}
+template <class T>
+void Deserialize(serializer* s, const string& name, T& object)
+{
+  typeof<T>()->Deserialize(s, name, &object);
+}
+
+template <class T>
+struct read_write
+{
+  static void read(serializer* s, const string& name, void* object) {}
+  static void write(serializer* s, const string& name, const void* object) {}
+};
+#define META_SPECIALIZE_READ_WRITE(T, FUNCTION)                                       \
+template <>                                                                           \
+struct read_write<T>                                                                  \
+{                                                                                     \
+  static void read(serializer* s, const string& name, void* object)                   \
+  {                                                                                   \
+    *(T*)object = s->Read##FUNCTION(name);                                            \
+  }                                                                                   \
+  static void write(serializer* s, const string& name, const void* object)            \
+  {                                                                                   \
+    s->Write##FUNCTION(name, *(T*)object);                                            \
+  }                                                                                   \
+};
+
+META_SPECIALIZE_READ_WRITE(bool, Bool)
+META_SPECIALIZE_READ_WRITE(char, Integer)
+META_SPECIALIZE_READ_WRITE(short, Integer)
+META_SPECIALIZE_READ_WRITE(int, Integer)
+META_SPECIALIZE_READ_WRITE(unsigned char, Integer)
+META_SPECIALIZE_READ_WRITE(unsigned short, Integer)
+META_SPECIALIZE_READ_WRITE(unsigned int, Integer)
+META_SPECIALIZE_READ_WRITE(float, Float)
+META_SPECIALIZE_READ_WRITE(double, Double)
+META_SPECIALIZE_READ_WRITE(string, String)
+
+template <class T>
+struct enum_read_write
+{
+  static void read(serializer* s, const string& name, void* object)
+  {
+    const string title = s->ReadEnum(name);
+    const type* type = typeof<T>();
+    const variant& value = type->getConstant(title).value;
+    *(T*)object = value.get_as<T>();
+  }
+  static void write(serializer* s, const string& name, const void* object)
+  {
+    const type* type = typeof<T>();
+    for (auto constant : type->constants())
+    {
+      assert(constant.type == type);
+
+      if (constant.value.get_as<T>() == *(T*)object)
+      {
+        s->WriteEnum(name, type, constant.name);
+        return;
+      }
+    }
+  }
+};
+
+template <class T, bool isEnum> struct read_write_selector;
+template <class T> struct read_write_selector<T, false> : public read_write<T>{};
+template <class T> struct read_write_selector<T, true> : public enum_read_write<T>{};
+
+template <class T> struct to_string
+{
+  static const string toString(const void*) { return string(); }
+};
+
+#define META_SPECIALIZE_TO_STRING(T)                  \
+template <> struct to_string<T>                       \
+{                                                     \
+  static const string toString(const void* object)    \
+  {                                                   \
+    ostringstream buff;                               \
+    buff << *((T*)object);                            \
+    return buff.str();                                \
+  }                                                   \
+};
+
+META_SPECIALIZE_TO_STRING(bool)
+META_SPECIALIZE_TO_STRING(char)
+META_SPECIALIZE_TO_STRING(short)
+META_SPECIALIZE_TO_STRING(int)
+META_SPECIALIZE_TO_STRING(unsigned char)
+META_SPECIALIZE_TO_STRING(unsigned short)
+META_SPECIALIZE_TO_STRING(unsigned int)
+META_SPECIALIZE_TO_STRING(float)
+META_SPECIALIZE_TO_STRING(double)
+META_SPECIALIZE_TO_STRING(string)
+
+template <class T> struct enum_to_string
+{
+  static const string toString(const void* object)
+  {
+    const type* type = typeof<T>();
+    for (auto constant : type->constants())
+    {
+      assert(constant.type == type);
+
+      if (constant.value.get_as<T>() == *(T*)object)
+      {
+        return constant.name;
+      }
+    }
+    return string();
+  }
+};
+template <class T, bool isEnum> struct to_string_selector;
+template <class T> struct to_string_selector<T, false> : public to_string<T>{};
+template <class T> struct to_string_selector<T, true> : public enum_to_string<T>{};
+
 }
